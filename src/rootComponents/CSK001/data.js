ab7d0cfa9a03ce0e73e073caad7072afb09677a7
Tường Chăm Chỉ <buivanthetuong123@gmail.com>
2026-02-05 16:53:38 +0700

    change some dir

--- src/rootComponents/CSK001/data.js
+++ src/rootComponents/CSK001/data.js
@@ -4,36 +4,37 @@ import { keepOnlyActionsCodeTimeFixedStt } from '../../components/ActionOrchestr
 import { CMD_Fetch } from '../../layouts/groupUtils.js';
 import { Logger } from '../../utils/logger.js';
 
-import group1_1 from '../../layouts/mode1/group1_1.js';
-import group2_1 from '../../layouts/mode1/group2_1.js';
-import group3_1 from '../../layouts/mode1/group3_1.js';
+import group1_1 from '../../layouts/group1/group1_1.js';
+import group1_2 from '../../layouts/group1/group1_2.js';
 
-import group1_2 from '../../layouts/mode2/group1_2.js';
-import group2_2 from '../../layouts/mode2/group2_2.js';
-import group3_2 from '../../layouts/mode2/group3_2.js';
-import group4_2 from '../../layouts/mode2/group4_2.js';
+import group2_1 from '../../layouts/group2/group2_1.js';
+import group2_2 from '../../layouts/group2/group2_2.js';
+import group2_3 from '../../layouts/group2/group2_3.js';
 
-import group2_3 from '../../layouts/mode3/group2_3.js';
-import group3_3 from '../../layouts/mode3/group3_3.js';
-import group4_3 from '../../layouts/mode3/group4_3.js';
-import group5_3 from '../../layouts/mode3/group5_3.js';
+import group3_1 from '../../layouts/group3/group3_1.js';
+import group3_2 from '../../layouts/group3/group3_2.js';
+import group3_3 from '../../layouts/group3/group3_3.js';
+
+import group4_2 from '../../layouts/group4/group4_2.js';
+import group4_3 from '../../layouts/group4/group4_3.js';
+
+import group5_3 from '../../layouts/group5/group5_3.js';
 
 const handlerMap = {
-  //chữ và ảnh
-  //chữ trên ảnh dưới
   group1_1: group1_1,
-  group2_1: group2_1,
-  group3_1: group3_1,
-  //chữ ảnh ảnh chữ
   group1_2: group1_2,
-  group2_2: group2_2,
-  group3_2: group3_2,
-  group4_2: group4_2,
-  //chữ ảnh và ảnh
 
+  group2_1: group2_1,
+  group2_2: group2_2,
   group2_3: group2_3,
+
+  group3_1: group3_1,
+  group3_2: group3_2,
   group3_3: group3_3,
+
+  group4_2: group4_2,
   group4_3: group4_3,
+
   group5_3: group5_3,
 };
 
@@ -96,10 +97,11 @@ DataFront.forEach((videoData) => {
   //   timeFixed: 0.5,
   // };
   // video.push(end_sound);
-  
+
   videoData01.push(video);
 });
 
+//log data video dau tien
 if (videoData01.length > 0) {
   Logger.debug('First video data:', videoData01[0]);
 }
